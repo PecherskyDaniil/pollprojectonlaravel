f1a46eded29de6e024f0ca29dfedef6a2fa485d9
Daniil Pechersky <cdtt38.PecherskyD@yandex.ru>
2024-10-31 11:48:04 +0800

    Fixed validation of form

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -12,7 +12,7 @@ class PollController extends Controller
 {
     //
     public function getPoll():View{
-        return view('poll',["name"=>"","email"=>"",'favoriteanimal'=>'','favoritefood'=>'','nameerror'=>'','emailerror'=>'','favanimerror'=>'','favfooderror'=>'','goodstatus'=>"",'badstatus'=>'']);
+        return view('poll',['goodstatus'=>"",'badstatus'=>'']);
     }
     public function getResults():View{
         return view('results');
@@ -22,49 +22,15 @@ class PollController extends Controller
         $email = $request->input('email');
         $favanim = $request->input('favoriteanimal');
         $favfood = $request->input('favoritefood');
-        $nameerror='';
-        $emailerror='';
-        $favanimerror='';
-        $favfooderror='';
-        $errors=0;
-        if (!is_valid($name)){
-            $nameerror='Name should have only letters!';
-            $errors+=1;
-        }
-        if (strlen($name)==0){
-            $nameerror='Name is empty!';
-            $name="";
-            $errors+=1;
-        }
-        if (strlen($email)==0){
-            $emailerror='Email is empty!';
-            $email="";
-            $errors+=1;
-        }
-        if (!is_valid($favanim)){
-            $favanimerror='Favorite animal should have only letters!';
-            $errors+=1;
-        }
-        if (strlen($favanim)==0){
-            $favanimerror='Favorite animal is empty!';
-            $favanim="";
-            $errors+=1;
-        }
-        if (!is_valid($favfood)){
-            $favfooderror='Favorite food should have only letters!';
-            $errors+=1;
-        }
-        if (strlen($favfood)==0){
-            $favfooderror='Favorite food is empty!';
-            $favfood="";
-            $errors+=1;
-        }
-        if ($errors>0){
-            return view('poll',["name"=>$name,"email"=>$email,'favoriteanimal'=>$favanim,'favoritefood'=>$favfood,'nameerror'=>$nameerror,'emailerror'=>$emailerror,'favanimerror'=>$favanimerror,'favfooderror'=>$favfooderror,'goodstatus'=>"",'badstatus'=>'Ошибка!']);
-        }
+        $validatedData = $request->validate([
+            'name' => ['required', 'max:255'],
+            'email' => ['required','email'],
+            'favoriteanimal' => ['required'],
+            'favoritefood' => ['required'],
+        ]);
         $data=["name"=>$name,"email"=>$email,"favoriteanimal"=>$favanim,"favoritefood"=>$favfood];
         $filename=md5($name.$email.$favanim.$favfood).".json";
         Storage::disk('public')->put($filename, json_encode($data,JSON_UNESCAPED_UNICODE));
-        return view('poll',["name"=>"","email"=>"",'favoriteanimal'=>'','favoritefood'=>'','nameerror'=>'','emailerror'=>'','favanimerror'=>'','favfooderror'=>'','goodstatus'=>"Данные добавлены успешно!",'badstatus'=>'']);
+        return view('poll',['goodstatus'=>"Данные добавлены успешно!",'badstatus'=>'']);
     }
 }
--- resources/views/poll.blade.php
+++ resources/views/poll.blade.php
@@ -3,10 +3,21 @@
         <div class="main-div">  
         <form method="POST">
             @csrf
-            Name: <input type="text" name="name" value="{{ $name }}"><div style="color:red;">{{$nameerror}}</div><br>
-            E-mail: <input type="text" name="email" value="{{ $email }}"><div style="color:red;">{{$emailerror}}</div><br>
-            Your favorite animal: <input type="text" name="favoriteanimal" value="{{ $favoriteanimal }}"><div style="color:red;">{{$favanimerror}}</div><br>
-            Your favorite food: <input type="text" name="favoritefood" value="{{ $favoritefood }}"><div style="color:red;">{{$favfooderror}}</div><br>
+            Name: <input type="text" name="name" value="{{ old('name') }}">@error('name')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror<br>
+            E-mail: <input type="text" name="email" value="{{ old('email') }}">
+            @error('email')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror<br>
+            Your favorite animal: <input type="text" name="favoriteanimal" value="{{ old('favoriteanumal') }}">
+            @error('favoriteanimal')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror<br>
+            Your favorite food: <input type="text" name="favoritefood" value="{{ old('favoritefood') }}">
+            @error('favoritefood')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror<br>
             <input type="submit">
         </form>
         </div>
